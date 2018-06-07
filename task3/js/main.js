a78f9f0bedc864f85ee44b4d9243c53849fb3e23
juanjuan <jiangjiangko@163.com>
2018-06-08 01:38:12 +0800

    page1

---
+++ task3/js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    var slider = $('.banner').unslider({
+        autoplay: true,
+        nav: true,
+        delay: 5000,
+    });
+    var sliderData = slider.data('unslider');
+
+    // 监听鼠标是否滑入 banner 区域，是就暂停，离开就继续自动轮播
+    $('.unslider-wrap li, .banner-nav li').on('mouseenter', function (e) {
+        sliderData.stop();
+    });
+    $('.unslider-wrap li, .banner-nav li').on('mouseleave', function (e) {
+        sliderData.start();
+    });
+
+    var $bannerNavLinks = $('.unslider-nav li');
+    $bannerNavLinks.on('mouseover', function () {
+        var dataIndex = $(this).data('slide');
+        sliderData.animate(dataIndex);
+    });
+    $('.banner').show();
+});
